80970a5c15c9c75e4bdba3add7b9f73d3e7890a9
Regina Steffen <reginasteffen@live.com>
2024-09-14 21:44:42 -0500

    created user model

--- models.py
+++ models.py
@@ -1,2 +1,28 @@
 """Models for Blogly."""
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, 
+                   primary_key=True,
+                   autoincrement=True)
+    
+    first_name = db.Column(db.String, 
+                         nullable=False)
+    
+    last_name = db.Column(db.String, 
+                          nullable=False)
+    
+    image_url = db.Column(db.String)
+
+    def __repr__(self):
+        user = self
+        return f"<User id ={user.id} first_name={user.first_name} last_name={user.last_name}>"
