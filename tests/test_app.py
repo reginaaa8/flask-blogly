57b8674ce3df0d1b399f45b8960665d5b5e1d2f9
Regina Steffen <reginasteffen@live.com>
2024-09-19 21:53:49 -0500

    created tests for routes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,74 +1,72 @@
 from unittest import TestCase
-
 from app import app
 from models import db, User
 
-# Use test database 
+# Configure the test database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['SQLALCHEMY_ECHO'] = False
-
-# Make Flask errors be real errors, rather than HTML pages with error info
 app.config['TESTING'] = True
 
-# This is a bit of hack, but don't use Flask DebugToolbar
-app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
-
-db.drop_all()
-db.create_all()
-
-class PetViewsTestCase(TestCase):
+class UserViewsTestCase(TestCase):
     """Tests for views for Users."""
 
+    @classmethod
+    def setUpClass(cls):
+        """Set up application context and initialize the database before tests."""
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+
     def setUp(self):
-        """Add sample user."""
+        """Add a sample user before each test."""
+        with app.app_context():
+            User.query.delete()  # Clear out any users from a previous test
+            user = User(first_name="Sandy", last_name="Cheeks", image_url="https://images.unsplash.com/photo-1470130623320-9583a8d06241?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3F1aXJyZWx8ZW58MHx8MHx8fDA%3D")
+            db.session.add(user)
+            db.session.commit()
 
-        User.query.delete()
+            self.user = user
 
-        user = User(first_name="Sandy", last_name="Cheeks", image_url="https://images.unsplash.com/photo-1470130623320-9583a8d06241?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3F1aXJyZWx8ZW58MHx8MHx8fDA%3D")
-        db.session.add(user)
-        db.session.commit()
+            self.user_id = user.id  # Store the user id for later tests
 
-        self.id = user.id
-        self.user = user
-    
     def tearDown(self):
-        """Clean up any fouled transaction."""
-
-        db.session.rollback()
+        """Clean up the session after each test."""
+        with app.app_context():
+            db.session.rollback()
 
     def test_list_users(self):
+        """Test if the homepage lists users."""
         with app.test_client() as client:
-            resp = client.get("/")
+            resp = client.get("/users")
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Sandy', html)
 
     def test_show_user(self):
+        """Test if the user detail page shows the correct user."""
         with app.test_client() as client:
-            resp = client.get(f"/users/{self.id}")
+            resp = client.get(f"/users/{self.user.id}")
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn(f'<h1>{self.first_name} {self.last_name}</h1>', html)
-            
+            self.assertIn(f'{self.user.first_name} {self.user.last_name}', html)
 
     def test_add_user(self):
+        """Test adding a new user."""
         with app.test_client() as client:
             new_user = {"first_name": "Mr.", "last_name": "Krabs", "image_url": "https://images.unsplash.com/photo-1533759587370-e5616b8b6209?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNyYWJ8ZW58MHx8MHx8fDA%3D"}
             resp = client.post("/users/new", data=new_user, follow_redirects=True)
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn("<li> Mr. Krabs </li>", html)
+            self.assertIn('<li><a href="/users/2">Mr. Krabs</a></li>', html)
 
-
-    def test_show_add_form(self):
+    def test_new_user_form(self):
+        """Test that the 'Add User' form is displayed."""
         with app.test_client() as client:
             resp = client.get('/users/new')
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn('<h1>Add a New User</h1>', html)
-
-            
