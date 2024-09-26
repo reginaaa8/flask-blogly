3ea3e5c2c0c09c7b245a78ea6c566f02a34b3af8
Regina Steffen <reginasteffen@live.com>
2024-09-25 21:29:42 -0500

    created Post model

--- models.py
+++ models.py
@@ -4,6 +4,8 @@ default_img = 'https://media.istockphoto.com/id/1337144146/vector/default-avatar
 
 from flask_sqlalchemy import SQLAlchemy
 
+import datetime
+
 db = SQLAlchemy()
 
 def connect_db(app):
@@ -31,3 +33,29 @@ class User(db.Model):
     def __repr__(self):
         u = self
         return f"<User id ={u.id} first_name={u.first_name} last_name={u.last_name}>"
+    
+class Post(db.Model):
+        """blog posts"""
+        __tablename__ = 'posts'
+
+        id =  db.Column(db.Integer, 
+                        primary_key=True,
+                        autoincrement=True)
+        
+        title = db.Column(db.String, 
+                          nullable=False)
+        
+        content = db.Column(db.String, 
+                            nullable=False)
+        
+        created_at = db.Column(db.DateTime, 
+                               nullable=False, 
+                               default=datetime.datetime.now)
+        
+        user_id = db.Column(db.Integer, 
+                            db.ForeignKey('users.id'), 
+                            nullable=False)
+        
+        def __repr__(self):
+            p = self
+            return f"<id ={p.id} title={p.title} created_at={p.created_at} user_id={p.user_id}>"
