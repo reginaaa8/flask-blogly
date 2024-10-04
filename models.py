d25a5e17543b6d1ef78710e1ed9032f664e342cb
Regina Steffen <reginasteffen@live.com>
2024-10-03 21:20:33 -0500

    created Tag model

--- models.py
+++ models.py
@@ -36,27 +36,46 @@ class User(db.Model):
         return f"<User id ={u.id} first_name={u.first_name} last_name={u.last_name}>"
     
 class Post(db.Model):
-        """blog posts"""
-        __tablename__ = 'posts'
+    """blog posts"""
+    __tablename__ = 'posts'
 
-        id =  db.Column(db.Integer, 
+    id =  db.Column(db.Integer, 
                         primary_key=True,
                         autoincrement=True)
         
-        title = db.Column(db.String, 
+    title = db.Column(db.String, 
                           nullable=False)
         
-        content = db.Column(db.String, 
+    content = db.Column(db.String, 
                             nullable=False)
         
-        created_at = db.Column(db.DateTime, 
+    created_at = db.Column(db.DateTime, 
                                nullable=False, 
                                default=datetime.datetime.now)
         
-        user_id = db.Column(db.Integer, 
+    user_id = db.Column(db.Integer, 
                             db.ForeignKey('users.id'), 
                             nullable=False)
         
-        def __repr__(self):
+    def __repr__(self):
             p = self
             return f"<id ={p.id} title={p.title} created_at={p.created_at} user_id={p.user_id}>"
+    
+class Tag(db.Model):
+     """tags for posts"""
+     __tablename__ = 'tags'
+
+     id = db.Column(db.Integer, 
+                    primary_key=True, 
+                    autoincrement=True)
+     
+     tag_name = db.Column(db.String, 
+                          unique=True,
+                          nullable=False)
+     
+     posts = db.relationship(
+          'Post', 
+          secondary="posts_tags",
+          backref="tags"
+     )
+    
